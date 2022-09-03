2ceaffd787922d0006f61304b8b866134ce475ff
Sergio Contreras <91096090+xWawi@users.noreply.github.com>
2022-09-03 14:53:44 -0500

    Add files via upload

---
+++ contarCaracteres.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+//Invertir palabras
+
+$cadena = readline("Digite una cadena");
+
+
+echo strrev("$cadena");
+
+//Contar palabras
+$contar = readline("Digite una palabra:");
+$caracter = ["a"];
+
+//Distancia de Hamming
+
+
+
+?>
